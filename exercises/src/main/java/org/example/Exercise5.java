0a0672b7d6af501496c147d2044820275f465201
Antonio Jimenez <ajimenez@rti.com>
2022-05-06 10:00:24 +0200

    Coin Change memoization

--- exercises/src/main/java/org/example/Exercise5.java
+++ exercises/src/main/java/org/example/Exercise5.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Exercise5 {
 
-    
-    static public int coinChange(int[] coins, int amount) {
+
+    static public int coinChangeImpl1(int[] coins, int amount) {
         int i = 1;
         int childResult = -1;
         if (amount == 0) {
@@ -15,13 +15,45 @@ public class Exercise5 {
         while (i <= coins.length && coins[coins.length - i] > amount) {
             i++;
         }
-        while (childResult == -1 && i <= coins.length) {
-            childResult = coinChange(coins, amount - coins[coins.length - i]);
+        while (i <= coins.length) {
+            childResult = coinChangeImpl1(coins, amount - coins[coins.length - i]);
             if (childResult == -1) {
                 i++;
-                continue;
             } else {
                 return 1 + childResult;
+            }
+        }
+        return -1;
+    }
+
+    static public int coinChangeImpl2(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        Arrays.sort(coins, 0, coins.length);
+        int memoize[] = new int[amount + 1];
+        Arrays.fill(memoize, -2);
+        return coinChangeImpl2Internal(memoize, coins, amount);
+    }
+
+    static public int coinChangeImpl2Internal(int[] memoize, int[] coins, int amount) {
+        int i = 1;
+        int childResult = -1;
+        if (amount == 0) {
+            return 0;
+        }
+        while (i <= coins.length && coins[coins.length - i] > amount) {
+            i++;
+        }
+        while (i <= coins.length) {
+            int newAmount = amount - coins[coins.length - i];
+            if (memoize[newAmount] == -2) {
+                memoize[newAmount] = coinChangeImpl2Internal(memoize, coins, newAmount);
+            }
+            if (memoize[newAmount] == -1) {
+                i++;
+            } else {
+                return 1 + memoize[newAmount];
             }
         }
         return -1;
--- exercises/src/test/java/org/example/Exercise5Test.java
+++ exercises/src/test/java/org/example/Exercise5Test.java
@@ -12,21 +12,28 @@ import static org.junit.Assert.assertTrue;
 public class Exercise5Test {
     @Test
     public void basic() {
-        assertEquals(3, Exercise5.coinChange(new int[]{1, 2, 5}, 11));
+        assertEquals(3, Exercise5.coinChangeImpl1(new int[]{1, 2, 5}, 11));
+        assertEquals(3, Exercise5.coinChangeImpl2(new int[]{1, 2, 5}, 11));
+
     }
 
     @Test
     public void fail() {
-        assertEquals(-1, Exercise5.coinChange(new int[]{2}, 3));
+        assertEquals(-1, Exercise5.coinChangeImpl1(new int[]{2}, 3));
+        assertEquals(-1, Exercise5.coinChangeImpl2(new int[]{2}, 3));
+
     }
+
 
     @Test
     public void amount0() {
-        assertEquals(0, Exercise5.coinChange(new int[]{1,2,5}, 0));
+        assertEquals(0, Exercise5.coinChangeImpl1(new int[]{1,2,5}, 0));
+        assertEquals(0, Exercise5.coinChangeImpl2(new int[]{1,2,5}, 0));
     }
 
     @Test
     public void advance() {
-        assertEquals(2, Exercise5.coinChange(new int[]{2,3,4}, 5));
+        assertEquals(2, Exercise5.coinChangeImpl1(new int[]{2,3,4}, 5));
+        assertEquals(2, Exercise5.coinChangeImpl2(new int[]{2,3,4}, 5));
     }
 }
