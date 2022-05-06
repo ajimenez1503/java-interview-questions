026db0f0fb8082b9bb02a0c90954905a6b132a29
Antonio Jimenez <ajimenez@rti.com>
2022-05-06 11:25:36 +0200

    Coin Change DP

--- exercises/src/main/java/org/example/Exercise5.java
+++ exercises/src/main/java/org/example/Exercise5.java
@@ -6,24 +6,29 @@ public class Exercise5 {
 
 
     static public int coinChangeImpl1(int[] coins, int amount) {
-        int i = 1;
-        int childResult = -1;
         if (amount == 0) {
             return 0;
         }
         Arrays.sort(coins, 0, coins.length);
-        while (i <= coins.length && coins[coins.length - i] > amount) {
-            i++;
+        return coinChangeImpl1Internal(coins, amount);
+    }
+
+    static public int coinChangeImpl1Internal(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
         }
-        while (i <= coins.length) {
-            childResult = coinChangeImpl1(coins, amount - coins[coins.length - i]);
-            if (childResult == -1) {
-                i++;
-            } else {
-                return 1 + childResult;
+
+        int result = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            if (coin > amount) {
+                break;
+            }
+            int tmpResult = coinChangeImpl1Internal(coins, amount - coin);
+            if (tmpResult != -1) {
+                result = Math.min(result, 1 + tmpResult);
             }
         }
-        return -1;
+        return result != Integer.MAX_VALUE ? result : -1;
     }
 
     static public int coinChangeImpl2(int[] coins, int amount) {
@@ -31,31 +36,48 @@ public class Exercise5 {
             return 0;
         }
         Arrays.sort(coins, 0, coins.length);
-        int memoize[] = new int[amount + 1];
-        Arrays.fill(memoize, -2);
-        return coinChangeImpl2Internal(memoize, coins, amount);
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        return coinChangeImpl2Internal(dp, coins, amount);
     }
 
-    static public int coinChangeImpl2Internal(int[] memoize, int[] coins, int amount) {
-        int i = 1;
-        int childResult = -1;
+    static public int coinChangeImpl2Internal(int[] dp, int[] coins, int amount) {
+        if (dp[amount] != Integer.MAX_VALUE) {
+            return dp[amount];
+        }
+
+        for (int coin : coins) {
+            if (coin > amount) {
+                break;
+            }
+            int result = coinChangeImpl2Internal(dp, coins, amount - coin);
+            if (result != -1) {
+                dp[amount] = Math.min(dp[amount], 1 + result);
+            }
+        }
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+    }
+
+    static public int coinChangeImpl3(int[] coins, int amount) {
         if (amount == 0) {
             return 0;
         }
-        while (i <= coins.length && coins[coins.length - i] > amount) {
-            i++;
-        }
-        while (i <= coins.length) {
-            int newAmount = amount - coins[coins.length - i];
-            if (memoize[newAmount] == -2) {
-                memoize[newAmount] = coinChangeImpl2Internal(memoize, coins, newAmount);
+        Arrays.sort(coins, 0, coins.length);
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin > i) {
+                    break;
+                }
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
-            if (memoize[newAmount] == -1) {
-                i++;
-            } else {
-                return 1 + memoize[newAmount];
-            }
         }
-        return -1;
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
+
 }
--- exercises/src/test/java/org/example/Exercise5Test.java
+++ exercises/src/test/java/org/example/Exercise5Test.java
@@ -14,14 +14,14 @@ public class Exercise5Test {
     public void basic() {
         assertEquals(3, Exercise5.coinChangeImpl1(new int[]{1, 2, 5}, 11));
         assertEquals(3, Exercise5.coinChangeImpl2(new int[]{1, 2, 5}, 11));
-
+        assertEquals(3, Exercise5.coinChangeImpl3(new int[]{1, 2, 5}, 11));
     }
 
     @Test
     public void fail() {
         assertEquals(-1, Exercise5.coinChangeImpl1(new int[]{2}, 3));
         assertEquals(-1, Exercise5.coinChangeImpl2(new int[]{2}, 3));
-
+        assertEquals(-1, Exercise5.coinChangeImpl3(new int[]{2}, 3));
     }
 
 
@@ -29,11 +29,20 @@ public class Exercise5Test {
     public void amount0() {
         assertEquals(0, Exercise5.coinChangeImpl1(new int[]{1,2,5}, 0));
         assertEquals(0, Exercise5.coinChangeImpl2(new int[]{1,2,5}, 0));
+        assertEquals(0, Exercise5.coinChangeImpl3(new int[]{1,2,5}, 0));
     }
 
     @Test
     public void advance() {
         assertEquals(2, Exercise5.coinChangeImpl1(new int[]{2,3,4}, 5));
         assertEquals(2, Exercise5.coinChangeImpl2(new int[]{2,3,4}, 5));
+        assertEquals(2, Exercise5.coinChangeImpl3(new int[]{2,3,4}, 5));
+    }
+
+    @Test
+    public void advance2() {
+        assertEquals(2, Exercise5.coinChangeImpl1(new int[]{1,3,4,5}, 7));
+        assertEquals(2, Exercise5.coinChangeImpl2(new int[]{1,3,4,5}, 7));
+        assertEquals(2, Exercise5.coinChangeImpl3(new int[]{1,3,4,5}, 7));
     }
 }
